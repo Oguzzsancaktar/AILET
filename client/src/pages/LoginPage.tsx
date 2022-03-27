8d1c670af0cec1eaf8b99c9c116fa678af4b0a7f
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-03-27 23:27:47 +0300

    Login page updated

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,48 +1,40 @@
-import { PageWrapper, SigninComponent, SignupComponent } from '@/components'
+import { Column, InnerWrapper, JustifyBetweenColumn, PageWrapper, SigninComponent, SignupComponent } from '@/components'
 import colors from '@/constants/colors'
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: ${colors.green.primary};
+`
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  max-height: 600px;
+  max-width: 300px;
+  margin: auto;
+  position: absolute;
+  padding: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  margin: auto;
+`
+
+const ChangeLoginType = styled.div``
+const ChangeLoginTypeText = styled.span``
+const ChangeLoginTypeButton = styled.button`
+  background-color: transparent;
+  border: none;
+  color: ${colors.yellow.primary};
+`
+
 const LoginPage: React.FC = () => {
-  const Wrapper = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background-color: ${colors.green.primary};
-  `
-
-  const Container = styled.div`
-    max-width: 1200px;
-    margin: auto;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    background: rebeccapurple;
-  `
-
-  const Column = styled.div`
-    width: fit-content;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-
-    background-color: aliceblue;
-  `
-  const Info = styled.div`
-    width: 100%;
-  `
-
-  const TextSecondary = styled.h3``
-  const TextPrimary = styled.h2``
-
-  const ChangeLoginType = styled.div``
-  const ChangeLoginTypeText = styled.span``
-  const ChangeLoginTypeButton = styled.button`
-    background-color: transparent;
-    border: none;
-    color: ${colors.yellow.primary};
-  `
-
   const [isSignup, setIsSignup] = useState(false)
 
   const handleClick = () => {
@@ -53,19 +45,17 @@ const LoginPage: React.FC = () => {
     <PageWrapper>
       <Wrapper>
         <Container>
-          <Column>
-            <Info>
-              <TextSecondary>Start For Free</TextSecondary>
-              <TextPrimary>Create a new account</TextPrimary>
+          <InnerWrapper>
+            <JustifyBetweenColumn>
+              {isSignup ? <SignupComponent /> : <SigninComponent />}
               <ChangeLoginType>
                 <ChangeLoginTypeText>
                   {isSignup ? 'Already have an account?' : "Don't have an account?"}
                 </ChangeLoginTypeText>
                 <ChangeLoginTypeButton onClick={handleClick}>{isSignup ? 'Sign in' : 'Sign up'}</ChangeLoginTypeButton>
               </ChangeLoginType>
-            </Info>
-            {isSignup ? <SignupComponent /> : <SigninComponent />}
-          </Column>
+            </JustifyBetweenColumn>
+          </InnerWrapper>
         </Container>
       </Wrapper>
     </PageWrapper>
