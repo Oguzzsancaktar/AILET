5973eccb1218ebb2a90224e71dfb7086b4fb1d92
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-04-16 00:13:09 +0300

    Dist folder removed and added to gitignore;

--- api/dist/users/dto/create.user.dto.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY3JlYXRlLnVzZXIuZHRvLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vdXNlcnMvZHRvL2NyZWF0ZS51c2VyLmR0by50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIn0=
