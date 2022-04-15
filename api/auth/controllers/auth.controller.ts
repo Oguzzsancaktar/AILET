73b46ab3fbccdd77d7a229dd746babc342640a7d
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-04-16 00:09:50 +0300

    Authentication System Implementation Done; - Auth Singleton Pattern created; - Auth controller added; - Auth Routes added; - Auth Middlewares added; -- JWT middleware; -- Auth Middlewate;

---
+++ api/auth/controllers/auth.controller.ts
@@ -0,0 +1,50 @@
+import express from 'express';
+import debug from 'debug';
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
+
+
+const log: debug.IDebugger = debug('app:auth-controller');
+
+/**
+* This value is automatically populated from .env, a file which you will have
+* to create for yourself at the root of the project.
+*
+* See .env.example in the repo for the required format.
+*/
+
+// @ts-expect-error
+const jwtSecret: string = process.env.JWT_SECRET;
+log(jwtSecret)
+const tokenExpirationInSeconds = 36000;
+
+class AuthController {
+
+  async createJWT(req: express.Request, res: express.Response) {
+    try {
+      const refreshId = req.body.userId + jwtSecret;
+      const salt = crypto.createSecretKey(crypto.randomBytes(16));
+      const hash = crypto
+        .createHmac('sha512', salt)
+        .update(refreshId)
+        .digest('base64');
+
+      req.body.refreshKey = salt.export();
+
+      const token = jwt.sign(req.body, jwtSecret, {
+        expiresIn: tokenExpirationInSeconds,
+      });
+
+      return res
+        .status(201)
+        .send({ accessToken: token, refreshToken: hash });
+
+    } catch (error) {
+      log('createJWT error: %O', error);
+      return res.status(500).send();
+    }
+  }
+
+}
+
+export default new AuthController()
---
+++ api/auth/middleware/auth.middleware.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import usersService from '../../users/services/users.service';
+import * as argon2 from 'argon2';
+
+class AuthMiddleware {
+
+  async verifyUserPassword(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    const user: any = await usersService.getUserByEmailWithPassword(
+      req.body.email
+    );
+    if (user) {
+      const passwordHash = user.password;
+      if (await argon2.verify(passwordHash, req.body.password)) {
+        req.body = {
+          userId: user._id,
+          email: user.email,
+          permissionFlags: user.permissionFlags,
+        };
+        return next();
+      }
+    }
+    // Giving the same message in both cases
+    // helps protect against cracking attempts:
+    res.status(400).send({ errors: ['Invalid email and/or password'] });
+  }
+
+}
+
+
+export default new AuthMiddleware()
