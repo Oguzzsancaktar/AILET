a556a647d0aa4c9aeba5553c15b83a058d2bfb86
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-04-16 00:17:15 +0300

    Authentication System Implementation Done; - User routes express-validator added; - User routes permission middlewares added; - User DAOS updated; - Permission flag hardcoded made dynamic; - User middleware updated validating mail and user demanding from request; - getUserByEmailWithPassword method added to user service;

--- api/users/users.routes.config.ts
+++ api/users/users.routes.config.ts
@@ -6,6 +6,9 @@ import UsersMiddleware from './middleware/users.middleware';
 import BodyValidationMiddleware from '../common/middleware/body.validation.middleware';
 import { body } from 'express-validator';
 
+import jwtMiddleware from '../auth/middleware/jwt.middleware';
+import permissionMiddleware from '../common/middleware/common.permission.middleware';
+import { PermissionFlag } from '../common/middleware/common.permissionflag.enum';
 export class UsersRoutes extends CommonRoutesConfig {
   constructor(app: express.Application) {
     super(app, 'UsersRoutes');
@@ -13,11 +16,20 @@ export class UsersRoutes extends CommonRoutesConfig {
 
   configureRoutes(): express.Application {
     this.app.route("/users")
-      .get(UsersController.listUsers)
+      .get(
+        jwtMiddleware.validJWTNeeded,
+        permissionMiddleware.permissionFlagRequired(PermissionFlag.ADMIN_PERMISSION),
+        UsersController.listUsers)
       .post(
-        UsersMiddleware.validateRequiredUserBodyFields,
+        // UsersMiddleware.validateRequiredUserBodyFields,
+        body('email').isEmail(),
+        body('password')
+          .isLength({ min: 5 })
+          .withMessage('Must include password (5+ characters)'),
+        BodyValidationMiddleware.verifyBodyFieldsErrors,
         UsersMiddleware.validateSameEmailDoesntExist,
-        UsersController.createUser)
+        UsersController.createUser
+      )
 
     this.app.param(`userId`, UsersMiddleware.extractUserId);
     // this.app.route("/users/:userId")
@@ -31,21 +43,41 @@ export class UsersRoutes extends CommonRoutesConfig {
     // })
     this.app
       .route(`/users/:userId`)
-      .all(UsersMiddleware.validateUserExists)
+      .all(
+        UsersMiddleware.validateUserExists,
+        jwtMiddleware.validJWTNeeded,
+        permissionMiddleware.onlySameUserOrAdminCanDoThisAction
+      )
       .get(UsersController.getUserById)
       .delete(UsersController.removeUser);
 
     this.app.put(`/users/:userId`, [
-      UsersMiddleware.validateRequiredUserBodyFields,
+      // UsersMiddleware.validateRequiredUserBodyFields,
+      body('email').isEmail(),
+      body('password')
+        .isLength({ min: 5 })
+        .withMessage('Must include password (5+ characters)'),
+      body('firstName').isString(),
+      body('lastName').isString(),
+      body('permissionFlags').isInt(),
+      BodyValidationMiddleware.verifyBodyFieldsErrors,
       UsersMiddleware.validateSameEmailBelongToSameUser,
       UsersController.put,
     ]);
 
     this.app.patch(`/users/:userId`, [
+      body('email').isEmail().optional(),
+      body('password')
+        .isLength({ min: 5 })
+        .withMessage('Password must be 5+ characters')
+        .optional(),
+      body('firstName').isString().optional(),
+      body('lastName').isString().optional(),
+      body('permissionFlags').isInt().optional(),
+      BodyValidationMiddleware.verifyBodyFieldsErrors,
       UsersMiddleware.validatePatchEmail,
       UsersController.patch,
     ]);
-
 
     return this.app;
   }
