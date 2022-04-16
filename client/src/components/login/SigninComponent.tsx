119cabdbce19131915eb9e81ea90783064e4a1c5
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-04-17 01:38:00 +0300

    FE authentication implementation; - Signin and Signup component handlers added;

--- client/src/components/login/SigninComponent.tsx
+++ client/src/components/login/SigninComponent.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Form, InputRegular, JustifyBetweenRow, Column, InputCheckbox } from '@/components'
+import { IUserLoginCredentials } from '@/models'
 
 interface Props {}
 const SigninComponent: React.FC<Props> = () => {
+  const [credentials, setCredentials] = useState<IUserLoginCredentials>()
+
+  const handleSignIn = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCredentials({ ...credentials, [event.target.name]: event.target.value } as IUserLoginCredentials)
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSignIn}>
       <Column>
-        <InputRegular placeholder="Username" type="text" />
-        <InputRegular placeholder="Password" type="password" />
+        <InputRegular onChange={handleInputChange} name="username" placeholder="Username" type="text" />
+        <InputRegular onChange={handleInputChange} name="password" placeholder="Password" type="password" />
       </Column>
       <JustifyBetweenRow>
         <InputCheckbox />
--- client/src/components/login/SignupComponent.tsx
+++ client/src/components/login/SignupComponent.tsx
@@ -1,21 +1,46 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import { Form, InputRegular, Button, Row } from '@/components'
 import { Column } from '../layout'
+import useAccessStore from '@/hooks/useAccessStore'
+import { IUserCreateDTO } from '@/models'
+import { createUser } from '@/services/userService'
 
 const SignupComponent = () => {
-  return (
-    <Form>
-      <Column>
-        <InputRegular placeholder="Name" type="text" />
-        <InputRegular placeholder="Surname" type="text" />
-        <InputRegular placeholder="Username" type="text" />
-        <InputRegular placeholder="Password" type="password" />
-        <InputRegular placeholder="Confirm Password" type="password" />
-        <InputRegular placeholder="E-mail" type="email" />
-        <InputRegular placeholder="Phone" type="tel" />
-      </Column>
+  const { useAppDispatch } = useAccessStore()
+  const dispatch = useAppDispatch()
+  const [signupFormValues, setSignupFormValues] = useState<IUserCreateDTO>()
 
+  const handleSignUp = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    try {
+      if (signupFormValues) {
+        const result = await createUser(signupFormValues)
+        console.log('result', result)
+      } else {
+        console.log('enter signupFormValues', signupFormValues)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSignupFormValues({ ...signupFormValues, [event.target.name]: event.target.value } as IUserCreateDTO)
+  }
+
+  return (
+    <Form onSubmit={handleSignUp}>
+      <Column>
+        <InputRegular onChange={handleInputChange} name="username" placeholder="Username" type="text" />
+        <InputRegular onChange={handleInputChange} name="password" placeholder="Password" type="password" />
+        <InputRegular
+          onChange={handleInputChange}
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          type="password"
+        />
+        <InputRegular onChange={handleInputChange} name="email" placeholder="E-mail" type="email" />
+      </Column>
       <Row>
         <Button>Sign Up</Button>
       </Row>
