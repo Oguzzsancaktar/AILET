c5a4bc08f0e1612ffc32ff7f31d4bfd39f3628aa
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-04-20 19:31:01 +0300

    [feature/login] BE login implementation; - User permission enums corrected; - User schema updated;

--- api/users/users.routes.config.ts
+++ api/users/users.routes.config.ts
@@ -1,37 +1,42 @@
-import { CommonRoutesConfig } from '../common/common.routes.config';
-import express from 'express';
-import UsersController from './controllers/users.controller';
-import UsersMiddleware from './middleware/users.middleware';
+import { CommonRoutesConfig } from "../common/common.routes.config";
+import express from "express";
+import UsersController from "./controllers/users.controller";
+import UsersMiddleware from "./middleware/users.middleware";
 
-import BodyValidationMiddleware from '../common/middleware/body.validation.middleware';
-import { body } from 'express-validator';
+import BodyValidationMiddleware from "../common/middleware/body.validation.middleware";
+import { body } from "express-validator";
 
-import jwtMiddleware from '../auth/middleware/jwt.middleware';
-import permissionMiddleware from '../common/middleware/common.permission.middleware';
-import { PermissionFlag } from '../common/middleware/common.permissionflag.enum';
+import jwtMiddleware from "../auth/middleware/jwt.middleware";
+import permissionMiddleware from "../common/middleware/common.permission.middleware";
+import { PermissionFlag } from "../common/middleware/common.permissionflag.enum";
 export class UsersRoutes extends CommonRoutesConfig {
   constructor(app: express.Application) {
-    super(app, 'UsersRoutes');
+    super(app, "UsersRoutes");
   }
 
   configureRoutes(): express.Application {
-    this.app.route("/users")
+    this.app
+      .route("/users")
       .get(
         jwtMiddleware.validJWTNeeded,
-        permissionMiddleware.permissionFlagRequired(PermissionFlag.ADMIN_PERMISSION),
-        UsersController.listUsers)
+        permissionMiddleware.permissionFlagRequired(
+          PermissionFlag.ADMIN_PERMISSION
+        ),
+        UsersController.listUsers
+      )
       .post(
         // UsersMiddleware.validateRequiredUserBodyFields,
-        body('username').isLength({ min: 6 })
-          .withMessage('Must include username (6+ characters)'),
-        body('email').isEmail(),
-        body('password')
+        body("username")
           .isLength({ min: 6 })
-          .withMessage('Must include password (6+ characters)'),
+          .withMessage("Must include username (6+ characters)"),
+        body("email").isEmail(),
+        body("password")
+          .isLength({ min: 6 })
+          .withMessage("Must include password (6+ characters)"),
         BodyValidationMiddleware.verifyBodyFieldsErrors,
         UsersMiddleware.validateSameEmailDoesntExist,
         UsersController.createUser
-      )
+      );
 
     this.app.param(`userId`, UsersMiddleware.extractUserId);
     // this.app.route("/users/:userId")
@@ -55,32 +60,31 @@ export class UsersRoutes extends CommonRoutesConfig {
 
     this.app.put(`/users/:userId`, [
       // UsersMiddleware.validateRequiredUserBodyFields,
-      body('email').isEmail(),
-      body('password')
+      body("email").isEmail(),
+      body("password")
         .isLength({ min: 6 })
-        .withMessage('Must include password (6+ characters)'),
-      body('firstName').isString(),
-      body('lastName').isString(),
-      body('permissionFlags').isInt(),
+        .withMessage("Must include password (6+ characters)"),
+      body("firstName").isString(),
+      body("lastName").isString(),
+      body("permissionFlags").isInt(),
       BodyValidationMiddleware.verifyBodyFieldsErrors,
       UsersMiddleware.validateSameEmailBelongToSameUser,
       UsersController.put,
     ]);
 
     this.app.patch(`/users/:userId`, [
-      body('email').isEmail().optional(),
-      body('password')
+      body("email").isEmail().optional(),
+      body("password")
         .isLength({ min: 6 })
-        .withMessage('Password must be 6+ characters')
+        .withMessage("Password must be 6+ characters")
         .optional(),
-      body('firstName').isString().optional(),
-      body('lastName').isString().optional(),
-      body('permissionFlags').isInt().optional(),
+      body("firstName").isString().optional(),
+      body("lastName").isString().optional(),
+      body("permissionFlags").isInt().optional(),
       BodyValidationMiddleware.verifyBodyFieldsErrors,
       UsersMiddleware.validatePatchEmail,
       UsersController.patch,
     ]);
-
 
     this.app.put(`/users/:userId/permissionFlags/:permissionFlags`, [
       jwtMiddleware.validJWTNeeded,
@@ -91,12 +95,11 @@ export class UsersRoutes extends CommonRoutesConfig {
       // /users/:userId, not anything beneath it in the hierarchy
 
       permissionMiddleware.permissionFlagRequired(
-        PermissionFlag.FREE_PERMISSION
+        PermissionFlag.USER_PERMISSION
       ),
       UsersController.updatePermissionFlags,
     ]);
 
     return this.app;
   }
-
 }
