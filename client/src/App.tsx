c3a577716389e666cecc5e6dade149ebf42c3c1b
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-04-20 19:27:47 +0300

    [feature/login] FE sass structure deprecated; - Global styled component styles added;

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-import './App.css'
+import GlobalStyle from './styles/GlobalStyle'
 
 const DashboardPage = lazy(() => import('./pages/DashboardPage'))
 const LoginPage = lazy(() => import('./pages/LoginPage'))
@@ -9,13 +9,13 @@ const LoginPage = lazy(() => import('./pages/LoginPage'))
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
+      <GlobalStyle />
       <Router>
         <Routes>
           <Route path="/" element={<DashboardPage />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
       </Router>
-
       <ToastContainer />
     </Suspense>
   )
